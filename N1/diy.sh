e51f99db4a8e7a880b3c832bce66a6f7a8fcf57b
haomeng133 <haomeng133@gmail.com>
2025-04-22 02:21:39 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Add packages
-#git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+ git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 #git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 
 # Update packages
 #rm -rf feeds/luci/applications/luci-app-passwall
-#cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+ cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # add luci-app-mosdns
 rm -rf feeds/packages/lang/golang
